07c12083e882844ebf235a90e25fc68a22d31e4a
Oliver Berry <info@littlemonsterdigital.co.uk>
2024-02-04 21:22:58 +0000

    Create base components and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
 
 import './style.scss'
 
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+
 const App = () => {
   return (
-    <div>App</div>
+    <Box
+    width="400"
+    m="auto"
+    sx={{ width:{ xl: '1440px' } }}>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={ <Home />} />
+            <Route path="/exercise/:id" element={ <ExerciseDetail />} />
+        </Routes>
+        <Footer />
+    </Box>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { ReactDOM} from "react"
-import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom"
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
@@ -8,4 +9,8 @@ const root = createRoot(
     document.getElementById('root')
 )
 
-root.render(<App />)
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
